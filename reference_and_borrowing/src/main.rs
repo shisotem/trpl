79b89f088527e4374548038d911d5c1e5419a3ca
shisotem <129283841+shisotem@users.noreply.github.com>
2025-05-04 02:03:18 +0900

    4.2. 参照と借用

--- reference_and_borrowing/src/main.rs
+++ reference_and_borrowing/src/main.rs
@@ -1,18 +1,40 @@
+// fn main() {
+//     let a = String::from("hello");
+//     let len = calculate_length(&a);
+//     println!("The length of '{}' is {}.", a, len); // 参照のお陰で、sを返すように関数を実装せずとも、ここでsが使える
+
+//     let mut b = String::from("hello");
+//     change(&mut b); // 可変な参照を生成する
+// }
+
+// fn calculate_length(s: &String) -> usize {
+//     // sは参照であり、所有者ではない (sへのmoveも起きていない)
+//     s.len()
+// }
+
+// // 可変な参照を受け入れる
+// fn change(some_string: &mut String) {
+//     some_string.push_str(", world");
+// }
+
 fn main() {
-    let a = String::from("hello");
-    let len = calculate_length(&a);
-    println!("The length of '{}' is {}.", a, len); // 参照のお陰で、sを返すように関数を実装せずとも、ここでsが使える
-
-    let mut b = String::from("hello");
-    change(&mut b); // 可変な参照を生成する
-}
-
-fn calculate_length(s: &String) -> usize {
-    // sは参照であり、所有者ではない (sへのmoveも起きていない)
-    s.len()
-}
-
-// 可変な参照を受け入れる
-fn change(some_string: &mut String) {
-    some_string.push_str(", world");
+    // 可変な参照の制約 ()=> データ競合を防ぐ)
+    let mut s = String::from("hello");
+    // // ok
+    let r1 = &s;
+    let r2 = &s;
+    // hello, hello
+    println!("{}, {}", r1, r2);
+    // // err
+    // let r1 = &mut s;
+    // let r2 = &s;
+    // println!("{}, {}", r1, r2);
+    // // err
+    // let r1 = &s;
+    // let r2 = &mut s;
+    // println!("{}, {}", r1, r2);
+    // // err
+    // let r1 = &mut s;
+    // let r2 = &mut s;
+    // println!("{}, {}", r1, r2);
 }
