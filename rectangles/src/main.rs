6d5e441f4da12f61f794802408ed62c5d48570d3
shisotem <129283841+shisotem@users.noreply.github.com>
2025-05-04 23:52:39 +0900

    5.2. 構造体を使ったプログラム例

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,11 +1,19 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
-    let rect1 = (30, 50); // 縦横がどちらか不明
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(rect1);
+        area(&rect1) // 借用 => mainは所有権を保ち、 rect1を使用し続けることができる
     );
 }
 
-fn area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
