ea03fcda54947a5a4f1a2138b3a91d69d9d31b2c
shisotem <129283841+shisotem@users.noreply.github.com>
2025-05-04 18:45:04 +0900

    4.3. スライス型

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -2,6 +2,12 @@ fn main() {
     let mut s = String::from("hello world");
     let word = first_word(&s);
     s.clear(); // s: ""とすると、word: 5はもはやバグのもと!!(同期されていない) => 文字列スライスを使う
+
+    // 文字列スライス: Stringの"一部への"参照
+    let mut s2 = String::from("hello world");
+    let word2 = first_word2(&s2);
+    s2.clear(); // 可変参照
+    // println!("the first word is: {}", word2); // compile err (word2: &str...不変参照)
 }
 
 fn first_word(s: &String) -> usize {
@@ -22,3 +28,13 @@ fn first_word(s: &String) -> usize {
 // 参照型である&u8から、参照を脱参照して、その内部の値であるu8型の値を取り出しているイメージ。
 // => 比較 (item == b' ') などを行う際に、都度、脱参照 (*item) を行う必要がなくなり簡潔になる。
 // cf. referece_and_borrowing/src/main.rs
+
+fn first_word2(s: &String) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+    &s[..]
+}
