da7deb82db0a02d5026e381ef115becf202c30e3
shisotem <129283841+shisotem@users.noreply.github.com>
2025-05-05 00:49:36 +0900

    5.3. メソッド記法

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -5,9 +5,13 @@ struct Rectangle {
 }
 
 impl Rectangle {
+    // // メソッド
     // self(ただし稀), &self, &mut selfを使い分ける
     fn area(&self) -> u32 {
         self.width * self.height
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
     }
 }
 
@@ -24,4 +28,15 @@ fn main() {
     // // println!("rect1 is {}", rect1); // {}: Display=エンドユーザ向け (注: 構造体にはDisplayトレイトが導出されていない)
     // println!("rect1 is {:?}", rect1); // {}: Debug=デバッグ向け
     // println!("rect1 is {:#?}", rect1);
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
