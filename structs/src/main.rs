e520fd22eb9d0626c4f93bfe3ca5cab3949acc97
shisotem <129283841+shisotem@users.noreply.github.com>
2025-05-04 23:28:17 +0900

    5.1. 構造体を定義しインスタンス化する

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -5,6 +5,10 @@ struct User {
     active: bool,
 }
 
+// tuple構造体
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
 fn main() {
     let mut user1 = User {
         email: String::from("someone@example.com"),
@@ -19,6 +23,9 @@ fn main() {
         username: String::from("anotherusername567"),
         ..user1
     };
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
 }
 
 fn build_user(email: String, username: String) -> User {
@@ -29,3 +36,6 @@ fn build_user(email: String, username: String) -> User {
         sign_in_count: 1,
     }
 }
+
+// ユニット様構造体 - トレイト
+// 構造体のフィールドに参照をもたせる場合 - ライフタイム指定子
