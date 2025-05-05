7d08ab3ce437964d15c15cc4afb83e6b4179055c
shisotem <129283841+shisotem@users.noreply.github.com>
2025-05-05 22:11:29 +0900

    6.2. Enumを定義する

--- an_enum/src/main.rs
+++ an_enum/src/main.rs
@@ -1,9 +1,24 @@
-enum IpAddr {
-    V4(u8, u8, u8, u8), // 構造体では不可能
-    V6(String),
+// struct QuitMessage; // ユニット構造体
+// struct MoveMessage {
+//     x: i32,
+//     y: i32,
+// }
+// struct WriteMessage(String); // タプル構造体
+// struct ChangeColorMessage(i32, i32, i32); // タプル構造体
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 }, // 匿名構造体を含む列挙子
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+impl Message {
+    fn call(&self) {
+        // Enum: メソッド定義可能
+    }
 }
 
 fn main() {
-    let home = IpAddr::V4(127, 0, 0, 1);
-    let loopback = IpAddr::V6(String::from("::1"));
+    let m = Message::Write(String::from("hello"));
+    m.call();
 }
