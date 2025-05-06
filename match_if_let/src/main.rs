d4942cf60eaf32caaf53844861de367657cb13c0
shisotem <129283841+shisotem@users.noreply.github.com>
2025-05-06 17:55:31 +0900

    6.2. match制御フロー演算子(if let)

--- match_if_let/src/main.rs
+++ match_if_let/src/main.rs
@@ -19,13 +19,37 @@ fn main() {
     let six = plus_one(five);
     let none = plus_one(None);
 
-    let some_u8_value = 0u8;
+    let some_u8_value = 0u8; // 0-255
     match some_u8_value {
         1 => println!("one"),
         3 => println!("three"),
         5 => println!("five"),
         7 => println!("seven"),
         _ => (), // _: どんな値にもマッチする
+    }
+
+    // // if let: 値が一つのパターンにマッチした時にコードを走らせ、他は無視するmatchへの糖衣構文
+    // // e.g. Some(3)のときのみ処理をしたい
+    let some_u8_value = Some(0u8);
+    // match some_u8_value {
+    //     Some(3) => println!("three"),
+    //     _ => (),
+    // }
+    if let Some(3) = some_u8_value {
+        println!("three");
+    }
+
+    let mut count = 0;
+    let coin2 = Coin::Quarter(UsState::Alabama);
+    // match coin2 {
+    //     // {:?}州のクォーターコイン
+    //     Coin::Quarter(state) => println!("State quarter from {:?}!!!", state),
+    //     _ => count += 1,
+    // }
+    if let Coin::Quarter(state) = coin2 {
+        println!("State quarter from {:?}!!!", state);
+    } else {
+        count += 1;
     }
 }
 
