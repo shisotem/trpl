089cf030d6c296690150e04f616a7b9f14051698
shisotem <129283841+shisotem@users.noreply.github.com>
2025-05-10 04:56:51 +0900

    7.3. モジュールツリーの要素を示すためのパス

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,18 +1,18 @@
 // lib.rs: restaurant(=package名)という名前の、ライブラリクレートのクレートルート(クレートルート=>crateモジュールを暗黙的に形成)
 // (https://chatgpt.com/share/681e3458-437c-8005-930c-45683a2a787c)
 
+// eat_at_restaurantとfront_of_houseは同じcrateモジュールで定義されている(兄弟)ので、pubがなくてもアクセスできる
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
     }
+}
 
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
+// 公開API
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+    self::front_of_house::hosting::add_to_waitlist();
 }
