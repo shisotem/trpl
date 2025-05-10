27089f8d10f56de91c0400fa7f6a72adcea3d310
shisotem <129283841+shisotem@users.noreply.github.com>
2025-05-10 21:12:31 +0900

    7.4. useキーワードでパスをスコープに持ち込む

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -71,24 +71,46 @@
 
 // --------------
 
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-    }
+// mod front_of_house {
+//     pub mod hosting {
+//         pub fn add_to_waitlist() {}
+//     }
+// }
+
+// use crate::front_of_house::hosting;
+
+// pub fn eat_at_restaurant() {
+//     hosting::add_to_waitlist();
+// }
+
+// mod example {
+//     // 構造体やenumなどなどはfull pathが慣例
+//     use std::collections::HashMap;
+
+//     fn example_fn() {
+//         let mut map = HashMap::new();
+//         map.insert(1, 2);
+//     }
+// }
+
+use std::fmt;
+use std::io;
+
+fn function1() -> fmt::Result {
+    // ...
 }
 
-use crate::front_of_house::hosting;
-
-pub fn eat_at_restaurant() {
-    hosting::add_to_waitlist();
+fn function2() -> io::Result<()> {
+    // ...
 }
 
-mod example {
-    // 構造体やenumなどなどはfull pathが慣例
-    use std::collections::HashMap;
+use std::fmt::Result;
+use std::io::Result as IoResult;
 
-    fn example_fn() {
-        let mut map = HashMap::new();
-        map.insert(1, 2);
-    }
+fn function1() -> Result {
+    // ...
+}
+
+fn function2() -> IoResult<()> {
+    // ...
 }
